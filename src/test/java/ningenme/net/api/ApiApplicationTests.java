1f84417ad607b5b89e9c6b92e7eeea0d85752233
ningenMe <amohkuru@gmail.com>
2021-06-16 03:30:17 +0900

    [add] first commit

---
+++ src/test/java/ningenme/net/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ningenme.net.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
