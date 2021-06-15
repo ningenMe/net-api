9da4327a2b64b279f5349a22f47ed38bfcabd1ab
ningenMe <amohkuru@gmail.com>
2021-06-16 03:58:02 +0900

    [add] healthcheck

---
+++ src/main/java/ningenme/net/api/application/controller/HealthCheckGetController/HealthCheckGetController.java
@@ -0,0 +1,15 @@
+package ningenme.net.api.application.controller.HealthCheckGetController;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckGetController {
+
+  @GetMapping("/v1/healthcheck")
+  ResponseEntity<String> getHealthCheck() {
+    return ResponseEntity.ok("ok");
+  }
+
+}
