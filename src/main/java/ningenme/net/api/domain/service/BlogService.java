de5678bb6ca45cfe91cab231f909b592b74281c3
ningenMe <amohkuru@gmail.com>
2021-07-14 05:09:49 +0900

    [modify]

--- src/main/java/ningenme/net/api/domain/service/BlogService.java
+++ src/main/java/ningenme/net/api/domain/service/BlogService.java
@@ -4,6 +4,7 @@ import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import ningenme.net.api.domain.entity.Blog;
+import ningenme.net.api.domain.entity.DiaryWithAround;
 import ningenme.net.api.domain.value.BlogType;
 import ningenme.net.api.domain.value.PostedDate;
 import ningenme.net.api.infrastructure.ameba.BlogAmebaRepository;
@@ -14,6 +15,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 
 @Service
 @RequiredArgsConstructor
@@ -54,5 +56,24 @@ public class BlogService {
     final Blog blog = blogMysqlRepository.getDiaryByPostedDate(postedDate);
     blogMysqlRepository.post(Collections.singletonList(blog.toBuilder().liked(blog.getLiked().plusOne()).build()));
     log.info(blog.getUrl().getValue()+" diary was liked");
+  }
+
+  public DiaryWithAround getDiary(@NonNull final PostedDate postedDate) {
+    final List<Blog> blogList = blogMysqlRepository.get(Collections.singletonList(BlogType.DIARY));
+    Blog next = null;
+    Blog curr = null;
+    Blog prev = null;
+    if(blogList.size() > 0) prev = blogList.get(0);
+    for (final Blog blog: blogList) {
+      next = curr;
+      curr = prev;
+      prev = blog;
+      if(Objects.equals(curr.getPostedDate(),postedDate)) {
+        return DiaryWithAround.of(prev,curr,next);
+      }
+    }
+    next = curr;
+    curr = prev;
+    return DiaryWithAround.of(null,curr,next);
   }
 }
