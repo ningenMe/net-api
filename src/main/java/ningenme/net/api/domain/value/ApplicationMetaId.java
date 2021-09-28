6ecb3a544d89f11b1b496bf3e176fd9c6d49b8a0
ningenMe <amohkuru@gmail.com>
2021-09-29 05:10:57 +0900

    [add] net-batch

--- src/main/java/ningenme/net/api/domain/value/ApplicationMetaId.java
+++ src/main/java/ningenme/net/api/domain/value/ApplicationMetaId.java
@@ -10,18 +10,19 @@ import java.util.Objects;
 @Getter
 @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
 public enum ApplicationMetaId {
-  NET_API("net-api"),
-  NET_FRONT("net-front"),
-  ;
-  @NonNull
-  private final String value;
+    NET_API("net-api"),
+    NET_FRONT("net-front"),
+    NET_BATCH("net-batch"),
+    ;
+    @NonNull
+    private final String value;
 
-  public static ApplicationMetaId of(final String value) {
-    for (ApplicationMetaId applicationMetaId : ApplicationMetaId.values()) {
-      if(Objects.equals(value, applicationMetaId.getValue())) {
-        return applicationMetaId;
-      }
+    public static ApplicationMetaId of(final String value) {
+        for (ApplicationMetaId applicationMetaId : ApplicationMetaId.values()) {
+            if(Objects.equals(value, applicationMetaId.getValue())) {
+                return applicationMetaId;
+            }
+        }
+        throw new RuntimeException(value + "is an unknown application name");
     }
-    throw new RuntimeException(value + "is an unknown application name");
-  }
 }
